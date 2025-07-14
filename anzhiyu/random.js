7061d73f081fbd2473dd136660f26bd157ca3d80
agilawood4 <1587792817@qq.com>
2025-07-14 22:49:35 +0800

    Site updated: 2025-07-14 22:49:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/14/字符串哈希/","2025/07/14/hello-world/"];function toRandomPost(){
+var posts=["2025/07/14/hello-world/","2025/07/14/字符串哈希/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
