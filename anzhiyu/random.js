8390489dadba9e4c0bf0a40963a66db9731e197a
agilawood4 <1587792817@qq.com>
2025-07-16 20:52:14 +0800

    Site updated: 2025-07-16 20:52:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/14/hello-world/","2025/07/15/Tire树/","2025/07/15/python-数据容器/","2025/07/14/字符串哈希/"];function toRandomPost(){
+var posts=["2025/07/16/KMP算法/","2025/07/15/Tire树/","2025/07/14/字符串哈希/","2025/07/14/hello-world/","2025/07/15/python-数据容器/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
