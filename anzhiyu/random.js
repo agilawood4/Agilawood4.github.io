0783e12cb5080c8b106dda1f4b1d241fba96e37b
agilawood4 <1587792817@qq.com>
2025-07-17 10:54:55 +0800

    Site updated: 2025-07-17 10:54:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/15/Tire树/","2025/07/16/KMP算法/","2025/07/14/hello-world/","2025/07/15/python-数据容器/","2025/07/16/python-函数/","2025/07/16/二分专题/","2025/07/14/字符串哈希/"];function toRandomPost(){
+var posts=["2025/07/14/hello-world/","2025/07/17/python-数据可视化/","2025/07/15/python-数据容器/","2025/07/16/python语法知识-1/","2025/07/16/python-函数/","2025/07/16/KMP算法/","2025/07/15/Tire树/","2025/07/17/矩阵快速幂/","2025/07/14/字符串哈希/","2025/07/16/二分专题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
